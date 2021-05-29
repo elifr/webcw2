9734e31d7478ad8ef6f96b09e5a3ade6e6f4df5f
Steven <steven2surrey@gmail.com>
2021-05-29 12:39:27 +0100

    added about us page content

--- interface-service/src/components/Navbar.js
+++ interface-service/src/components/Navbar.js
@@ -19,7 +19,11 @@ function Navbar() {
                 <Link to="#" className='menu-bars'>
                     <FaIcons.FaBars onClick={showSidebar}/>
                 </Link>
+                <h1 className='mainTitle'>
+                Uni Chat
+                </h1>
             </div>
+
             
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <ul className='nav-menu-items' onClick={showSidebar}>
--- interface-service/src/pages/About.js
+++ interface-service/src/pages/About.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 function About() {
     return (
-        <div className='about'>
-            <p>
+            <body className='aboutSubContent'>
                 <h1>About Us</h1>
-                <h5>decription text</h5>
-            </p>
-        </div>
+                <h6>Uni Chat provides a distraction free
+                way for students to communicate
+                using the preexisting university network.</h6>
+                <h4>HOME</h4>
+                <h6>The home page provides a main start point for the user.</h6>
+                <h4>FEED</h4>
+                <h6>Feed provides a way to send and view posts that are viewable across all courses.</h6> 
+                <h4>CHATS</h4>
+                <h6>Chats provides a way for users to communicate to others on their modules as well as on their course.</h6>
+            </body>
     )
 }
 
