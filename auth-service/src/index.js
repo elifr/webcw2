90a40bf2f9890bfa8ead26855d8a3364807e238d
“elifr“ <elifrafet13@gmail.com>
2021-06-01 11:12:02 +0100

    merging

---
+++ auth-service/src/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const mongoose = require("mongoose");
+const currentUserRouter = require("./routes/current-user");
+const signInRouter = require("./routes/signin");
+const signOutRouter = require("./routes/signout");
+const signUpRouter = require("./routes/signup");
+
+const app = express();
+app.use(cors({credentials: true, origin: "http://localhost:8000"}));
+app.set("trust proxy", true);
+app.use(bodyParser.json());
+//initiating the cookie session library
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public/views"));
+app.set("view engine", "ejs");
+
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+
+//connecting to the database
+const start = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://noodleh:uk0cY811ic@cluster0.m6ge9.mongodb.net/webcw2?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!");
+  });
+};
+
+start();
